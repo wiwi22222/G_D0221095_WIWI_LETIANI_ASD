4bab5095802ba9c8232f0ef9e4beca52e80d2f4f
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2022-03-21 00:52:11 +0800

    komentar

---
+++ set.py
@@ -0,0 +1,17 @@
+set = {'wiwi',18,1.60,True}
+
+#perulangan
+for i in (set) :
+    print(i)
+
+#menghapus nilai pada set
+set.discard(1.60)
+print(set)
+
+#menambahkan nilai pada set
+set.add("informatika")
+print(set)
+
+#mengupdate nilai pada set
+set = {18}
+print(set[0])
